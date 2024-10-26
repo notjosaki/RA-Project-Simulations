958132a0a9d50dbcb767ea67f1f6cb704e717ee4
max <1631550@uab.cat>
2024-10-26 09:55:50 +0200

    Taichi

--- Simulations/main.py
+++ Simulations/main.py
@@ -19,13 +19,13 @@ etiqueta_bienvenida = tk.Label(ventana, text="Visualizacions de fenomens fisics"
 etiqueta_bienvenida.pack(pady=20)
 
 
-btn_100spheres = tk.Button(ventana, text="100 Esferas Rebotando", width=30, bg="#ADD8E6", command=lambda: ejecutar_visualizacion("Simulations/100spheres_bouncing.py"))
+btn_100spheres = tk.Button(ventana, text="100 Esferas Rebotando", width=30, bg="#ADD8E6", command=lambda: ejecutar_visualizacion("/Simulations/100spheres_bouncing.py"))
 btn_100spheres.pack(pady=10)
 
-btn_acelerador = tk.Button(ventana, text="Acelerador de Partículas", width=30, bg="#ADD8E6", command=lambda: ejecutar_visualizacion("Simulations/Accelerador_de_particules.py"))
+btn_acelerador = tk.Button(ventana, text="Acelerador de Partículas", width=30, bg="#ADD8E6", command=lambda: ejecutar_visualizacion("/Simulations/Accelerador_de_particules.py"))
 btn_acelerador.pack(pady=10)
 
-btn_dos_cubos = tk.Button(ventana, text="Colisión de Dos Cubos", width=30, bg="#ADD8E6", command=lambda: ejecutar_visualizacion("Simulations/two_cubes_colisioning.py"))
+btn_dos_cubos = tk.Button(ventana, text="Colisión de Dos Cubos", width=30, bg="#ADD8E6", command=lambda: ejecutar_visualizacion("/Simulations/two_cubes_colisioning.py"))
 btn_dos_cubos.pack(pady=10)
 
 
